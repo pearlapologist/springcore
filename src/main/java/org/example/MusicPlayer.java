e245ece73c9dadc1edb1da4927205349cb16854e
wirigiwewe <addict.hamster@gmail,com>
2022-09-27 16:20:53 +0600

    dependecy injection 2

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
+
+
+    public MusicPlayer() {
+    }
 
     public MusicPlayer(Music music){
         this.music = music;
@@ -9,4 +15,25 @@ public class MusicPlayer {
     public void playMusic(){
         System.out.println("Playing " + music.getSong());
     }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
 }
